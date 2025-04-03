07b4824d3e592b22b31cd8c752d10f6a43edb8fa
dextersjw <dextersjw@gmail.com>
2025-04-03 11:13:33 +0800

    initial commit

---
+++ src/main/java/challenge/tech/crud_auth/qr_code_service/QrCodeServiceApplication.java
@@ -0,0 +1,13 @@
+package challenge.tech.crud_auth.qr_code_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCodeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/challenge/tech/crud_auth/qr_code_service/QrCodeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.tech.crud_auth.qr_code_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
