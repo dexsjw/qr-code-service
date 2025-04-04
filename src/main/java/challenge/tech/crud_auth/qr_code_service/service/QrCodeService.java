744fd3cad2f7d55c97dd81affecacf0e7e66f0c2
dextersjw <dextersjw@gmail.com>
2025-04-04 10:21:53 +0800

    created QrCodeService

---
+++ src/main/java/challenge/tech/crud_auth/qr_code_service/service/QrCodeService.java
@@ -0,0 +1,11 @@
+package challenge.tech.crud_auth.qr_code_service.service;
+
+import challenge.tech.crud_auth.qr_code_service.dto.UploadSuccessResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface QrCodeService {
+
+    UploadSuccessResponseDto processUploadedQrCode(MultipartFile qrCodeFile);
+
+}
