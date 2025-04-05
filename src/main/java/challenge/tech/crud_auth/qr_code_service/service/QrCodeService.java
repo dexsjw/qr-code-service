2178607f818dc46241ced126304e6bdef2b68ad5
dextersjw <dextersjw@gmail.com>
2025-04-06 05:21:13 +0800

    updated QrCodeService with new methods

--- src/main/java/challenge/tech/crud_auth/qr_code_service/service/QrCodeService.java
+++ src/main/java/challenge/tech/crud_auth/qr_code_service/service/QrCodeService.java
@@ -1,10 +1,18 @@
 package challenge.tech.crud_auth.qr_code_service.service;
 
-import challenge.tech.crud_auth.qr_code_service.dto.UploadSuccessResponseDto;
+import challenge.tech.crud_auth.qr_code_service.dto.BaseResponseDto;
+import challenge.tech.crud_auth.qr_code_service.dto.QrCodeDataResponseDto;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface QrCodeService {
 
-    UploadSuccessResponseDto processUploadedQrCode(MultipartFile qrCodeFile);
+    QrCodeDataResponseDto processUploadedQrCodeFile(MultipartFile qrCodeFile);
+    BaseResponseDto processUploadedQrCodeFiles(List<MultipartFile> qrCodeFiles);
+    QrCodeDataResponseDto searchQrCodeData(Long id);
+    byte[] searchQrCodeImage(Long id);
+    BaseResponseDto deleteQrCodeData(Long id);
+    BaseResponseDto deleteAllQrCodeData();
 
 }
