c785b755ce43476424788fc78860c2f1c8095c88
dextersjw <dextersjw@gmail.com>
2025-04-06 05:25:01 +0800

    updated QrCodeController

--- src/main/java/challenge/tech/crud_auth/qr_code_service/controller/QrCodeController.java
+++ src/main/java/challenge/tech/crud_auth/qr_code_service/controller/QrCodeController.java
@@ -1,18 +1,16 @@
 package challenge.tech.crud_auth.qr_code_service.controller;
 
-import challenge.tech.crud_auth.qr_code_service.dto.UploadSuccessResponseDto;
+import challenge.tech.crud_auth.qr_code_service.dto.BaseResponseDto;
+import challenge.tech.crud_auth.qr_code_service.dto.QrCodeDataResponseDto;
 import challenge.tech.crud_auth.qr_code_service.service.QrCodeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -26,8 +24,33 @@ public class QrCodeController {
     }
 
     @PostMapping(path = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<UploadSuccessResponseDto> processUploadedQrCode(@RequestPart MultipartFile qrCodeFile) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(qrCodeService.processUploadedQrCode(qrCodeFile));
+    public ResponseEntity<QrCodeDataResponseDto> processUploadedQrCodeFile(@RequestPart MultipartFile qrCodeFile) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(qrCodeService.processUploadedQrCodeFile(qrCodeFile));
+    }
+
+    @PostMapping(path = "/upload/multiple", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<BaseResponseDto> processUploadedQrCodeFiles(@RequestPart List<MultipartFile> qrCodeFiles) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(qrCodeService.processUploadedQrCodeFiles(qrCodeFiles));
+    }
+
+    @GetMapping(path = "/data/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<QrCodeDataResponseDto> searchQrCodeData(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(qrCodeService.searchQrCodeData(id));
+    }
+
+    @GetMapping(path = "/image/{id}", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> searchQrCodeImage(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(qrCodeService.searchQrCodeImage(id));
+    }
+
+    @DeleteMapping(path = "/data/{id}")
+    public ResponseEntity<BaseResponseDto> deleteQrCodeData(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(qrCodeService.deleteQrCodeData(id));
+    }
+
+    @DeleteMapping(path = "/data/all")
+    public ResponseEntity<BaseResponseDto> deleteAllQrCodeData() {
+        return ResponseEntity.status(HttpStatus.OK).body(qrCodeService.deleteAllQrCodeData());
     }
 
 }
