66ded95d99451720f57c35fadf7e3a0ccbf28ed8
dextersjw <dextersjw@gmail.com>
2025-04-06 17:51:07 +0800

    created GlobalExceptionHandler

---
+++ src/main/java/challenge/tech/crud_auth/qr_code_service/controller/GlobalExceptionHandler.java
@@ -0,0 +1,82 @@
+package challenge.tech.crud_auth.qr_code_service.controller;
+
+import challenge.tech.crud_auth.qr_code_service.dto.BaseResponseDto;
+import challenge.tech.crud_auth.qr_code_service.dto.ErrorResponseDto;
+import challenge.tech.crud_auth.qr_code_service.exception.QrCodeDataNotFoundException;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.WriterException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(QrCodeDataNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleQrCodeDataNotFoundException(QrCodeDataNotFoundException ex) {
+        log.error(ex.getMessage(), ex.getCause(), ex.getStackTrace());
+        ErrorResponseDto errorResponseDto = ErrorResponseDto.ErrorResponseDtoBuilder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.NOT_FOUND)
+                .message("Error finding QR code data")
+                .errorMessage(ex.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponseDto);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleNotFoundException(NotFoundException ex) {
+        log.error(ex.getMessage(), ex.getCause(), ex.getStackTrace());
+        ErrorResponseDto errorResponseDto = ErrorResponseDto.ErrorResponseDtoBuilder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.NOT_FOUND)
+                .message("Error finding QR code in image")
+                .errorMessage(ex.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponseDto);
+    }
+
+    @ExceptionHandler(WriterException.class)
+    public ResponseEntity<ErrorResponseDto> handleWriterException(WriterException ex) {
+        log.error(ex.getMessage(), ex.getCause(), ex.getStackTrace());
+        ErrorResponseDto errorResponseDto = ErrorResponseDto.ErrorResponseDtoBuilder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .message("Error writing to OutputStream")
+                .errorMessage(ex.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponseDto);
+    }
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<ErrorResponseDto> handleIOException(IOException ex) {
+        log.error(ex.getMessage(), ex.getCause(), ex.getStackTrace());
+        ErrorResponseDto errorResponseDto = ErrorResponseDto.ErrorResponseDtoBuilder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .message("Error occurred for I/O operation")
+                .errorMessage(ex.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponseDto);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleException(Exception ex) {
+        log.error("An unknown Exception has occurred!");
+        log.error(ex.getMessage(), ex.getCause(), ex.getStackTrace());
+        ErrorResponseDto errorResponse = ErrorResponseDto.ErrorResponseDtoBuilder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .message("Something went wrong")
+                .errorMessage(ex.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+
+}
