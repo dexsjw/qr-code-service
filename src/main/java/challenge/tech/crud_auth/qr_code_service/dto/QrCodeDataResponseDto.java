b725ba39500023fc691b7a3ca203e50310a8e8a5
dextersjw <dextersjw@gmail.com>
2025-04-06 17:54:15 +0800

    updated QrCodeDataResponseDto with qrCodeDataList field

--- src/main/java/challenge/tech/crud_auth/qr_code_service/dto/QrCodeDataResponseDto.java
+++ src/main/java/challenge/tech/crud_auth/qr_code_service/dto/QrCodeDataResponseDto.java
@@ -1,24 +1,31 @@
 package challenge.tech.crud_auth.qr_code_service.dto;
 
 import challenge.tech.crud_auth.qr_code_service.record.QrCodeDataRecord;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatusCode;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-@EqualsAndHashCode(callSuper = true)
 @Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class QrCodeDataResponseDto extends BaseResponseDto {
 
     private QrCodeDataRecord qrCodeData;
+    private List<QrCodeDataRecord> qrCodeDataList;
 
     @Builder(builderMethodName = "QrCodeDataResponseDtoBuilder")
     public QrCodeDataResponseDto(LocalDateTime timestamp, HttpStatusCode status, String message,
-                                    QrCodeDataRecord qrCodeData) {
+                                    QrCodeDataRecord qrCodeData, List<QrCodeDataRecord> qrCodeDataList) {
         super(timestamp, status, message);
         this.qrCodeData = qrCodeData;
+        this.qrCodeDataList = qrCodeDataList;
     }
 
 }
