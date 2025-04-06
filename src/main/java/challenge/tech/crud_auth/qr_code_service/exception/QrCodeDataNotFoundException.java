d7816881f03d4bc759aa0ca5369a12c50b8044ee
dextersjw <dextersjw@gmail.com>
2025-04-06 17:35:51 +0800

    updated QrCodeDataNotFoundException message

--- src/main/java/challenge/tech/crud_auth/qr_code_service/exception/QrCodeDataNotFoundException.java
+++ src/main/java/challenge/tech/crud_auth/qr_code_service/exception/QrCodeDataNotFoundException.java
@@ -2,6 +2,6 @@ package challenge.tech.crud_auth.qr_code_service.exception;
 
 public class QrCodeDataNotFoundException extends RuntimeException {
     public QrCodeDataNotFoundException(Long id) {
-        super("QR Code data could not be found for id: " + id);
+        super("QR code data could not be found for id: " + id);
     }
 }
