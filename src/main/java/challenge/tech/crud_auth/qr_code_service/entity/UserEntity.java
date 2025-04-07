10238dd99d18428cc58e59ef77db4fda116cdda2
dextersjw <dextersjw@gmail.com>
2025-04-07 11:06:38 +0800

    created User entity and repository

---
+++ src/main/java/challenge/tech/crud_auth/qr_code_service/entity/UserEntity.java
@@ -0,0 +1,40 @@
+package challenge.tech.crud_auth.qr_code_service.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_role_id")
+    )
+    @JsonManagedReference
+    private List<UserRoleEntity> userRoles;
+
+}
